0087b7632afe141fc4ec1b0a4f79623f764a237a
坂上計介 <iwayasunset@gmail.com>
2020-04-02 12:50:49 +0900

    dbSetting
    shopモデル、テーブル、seed_fuデモデータ投入

---
+++ db/fixtures/shop.rb
@@ -0,0 +1,78 @@
+Shop.seed do |s|
+  s.id = 1
+  s.name = "エンジニアカフェ"
+  s.address = "〒810-0001 福岡県福岡市中央区天神１丁目１５−３０"
+  s.mood = "とても静か"
+  s.price_id = 1
+  s.power_id = 1
+  s.open_time = "9時"
+  s.close_time = "21時"
+  s.holiday = "最終月曜日"
+  s.latitude = "33.593152"
+  s.longitude = "130.401884"
+end
+Shop.seed do |s|
+  s.id = 2
+  s.name = "カフェベローチェ"
+  s.address = "〒810-0001 福岡県福岡市中央区天神１丁目１２−２０"
+  s.mood = "とても静か"
+  s.price_id = 1
+  s.power_id = 1
+  s.open_time = "9時"
+  s.close_time = "21時"
+  s.holiday = "最終月曜日"
+  s.latitude = "33.592187"
+  s.longitude = "130.399474"
+end
+Shop.seed do |s|
+  s.id = 3
+  s.name = "BOOK & CO."
+  s.address = "〒810-0001 福岡県福岡市中央区天神４丁目４−１１"
+  s.mood = "とても静か"
+  s.price_id = 1
+  s.power_id = 1
+  s.open_time = "9時"
+  s.close_time = "21時"
+  s.holiday = "最終月曜日"
+  s.latitude = "33.593852"
+  s.longitude = "130.39835"
+end
+Shop.seed do |s|
+  s.id = 4
+  s.name = "タリーズ 天神店"
+  s.address = "〒810-0001 福岡県福岡市中央区天神２丁目１４−３３"
+  s.mood = "とても静か"
+  s.price_id = 1
+  s.power_id = 1
+  s.open_time = "9時"
+  s.close_time = "21時"
+  s.holiday = "最終月曜日"
+  s.latitude = "33.591618"
+  s.longitude = "130.396871"
+end
+Shop.seed do |s|
+  s.id = 5
+  s.name = "タリーズ 新天町店"
+  s.address = "〒810-0001 福岡県福岡市中央区天神２丁目７−２４１"
+  s.mood = "とても静か"
+  s.price_id = 1
+  s.power_id = 1
+  s.open_time = "9時"
+  s.close_time = "21時"
+  s.holiday = "最終月曜日"
+  s.latitude = "33.590074"
+  s.longitude = "130.397071"
+end
+Shop.seed do |s|
+  s.id = 6
+  s.name = "スタバ 天神VIORO店"
+  s.address = "〒810-0001 福岡県福岡市中央区天神２丁目１０−３"
+  s.mood = "とても静か"
+  s.price_id = 1
+  s.power_id = 1
+  s.open_time = "9時"
+  s.close_time = "21時"
+  s.holiday = "最終月曜日"
+  s.latitude = "33.589645 "
+  s.longitude = "130.398415"
+end
