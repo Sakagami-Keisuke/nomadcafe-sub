3333d5b0a774397ec8479b99672486c2c7dc3bc8
坂上計介 <iwayasunset@gmail.com>
2020-05-24 21:28:05 +0900

    update

--- db/schema.rb
+++ db/schema.rb
@@ -30,7 +30,7 @@ ActiveRecord::Schema.define(version: 2020_04_09_052940) do
 
   create_table "inquiries", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
-    t.string "message"
+    t.text "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
